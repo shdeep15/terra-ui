833ac1115c2946c3aedebb5ec848292756aef9f8
tz043867 <tao.zhang@cerner.com>
2017-06-14 11:22:26 -0500

    Add express server script

---
+++ script/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8081;
+// path to webpack built path
+const buildPath = path.join(__dirname, '../public/webpack/development');
+
+app.use('/static', express.static(buildPath));
+app.get('/', (req, res) => res.redirect('/static'));
+app.listen(port);
+console.log(`Listening ${port}`);
+console.log(`Production Environment: ${process.env.NODE_ENV === 'production'}`);
