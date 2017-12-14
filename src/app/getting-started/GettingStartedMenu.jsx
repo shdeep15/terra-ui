b5192785fff369420c0b36e1cf14a4fbc39fb05c
Kasper,Dave <Dave.Kasper@Cerner.com>
2017-12-14 15:44:26 -0600

    update getting started menu.

--- src/app/getting-started/GettingStartedMenu.jsx
+++ src/app/getting-started/GettingStartedMenu.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-/* eslint-disable import/no-extraneous-dependencies */
+import PropTypes from 'prop-types';
+import { Link, NavLink } from 'react-router-dom';
+import ContentContainer from 'terra-content-container';
 import List from 'terra-list';
 import Heading from 'terra-heading';
-/* eslint-enable import/no-extraneous-dependencies */
+import MenuToolbar from '../common/menu/MenuToolbar';
 
-const scrollToComponent = (id) => {
-  document.querySelector(id).scrollIntoView();
+import styles from './GettingStartedMenu.scss';
+
+const GettingStartedMenu = ({ routingStackDelegate, layoutConfig }) => {
+  const scrollToComponent = (id) => {
+    if (layoutConfig.menuIsOpen && layoutConfig.toggleMenu) {
+      layoutConfig.toggleMenu();
+    }
+    document.querySelector(id).scrollIntoView();
+  };
+
+  return (
+    <ContentContainer
+      header={<MenuToolbar text="Getting Started" routingStackDelegate={routingStackDelegate} />}
+      fill
+    >
+      <List className={styles['menu']}>
+        <List.Item content={<Link to="/getting-started#what-is-terra-" onClick={() => { scrollToComponent('#what-is-terra-'); }} >What is Terra?</Link>} />
+        <List.Item content={<Link to="/getting-started#features" onClick={() => { scrollToComponent('#features'); }} >Features</Link>} />
+        <List.Item content={<Link to="/getting-started#installing-components" onClick={() => { scrollToComponent('#installing-components'); }} >Installing Components</Link>} />
+        <List.Item content={<Link to="/getting-started#dependencies" onClick={() => { scrollToComponent('#dependencies'); }} >Dependencies</Link>} />
+        <List.Item content={<Link to="/getting-started#basic-usage" onClick={() => { scrollToComponent('#basic-usage'); }} >Basic Usage</Link>} />
+        <List.Item content={<Link to="/getting-started#configuring-webpack" onClick={() => { scrollToComponent('#configuring-webpack'); }} >Configuring Webpack</Link>} />
+        <List.Item content={<Link to="/getting-started#internationalization-i18n-and-localization-l10n-" onClick={() => { scrollToComponent('#internationalization-i18n-and-localization-l10n-'); }} >I18n</Link>} />
+        <List.Item content={<Link to="/getting-started#theming" onClick={() => { scrollToComponent('#theming'); }} >Theming</Link>} />
+      </List>
+    </ContentContainer>
+  );
 };
 
-const GettingStartedMenu = () => (
-  <List>
-    <List.Item content={<Heading level={1} size="large">Getting Started</Heading>} />
-    <List.Item content={<Link to="/getting-started#what-is-terra-" onClick={() => { scrollToComponent('#what-is-terra-'); }} >What is Terra?</Link>} />
-    <List.Item content={<Link to="/getting-started#features" onClick={() => { scrollToComponent('#features'); }} >Features</Link>} />
-    <List.Item content={<Link to="/getting-started#installing-components" onClick={() => { scrollToComponent('#installing-components'); }} >Installing Components</Link>} />
-    <List.Item content={<Link to="/getting-started#dependencies" onClick={() => { scrollToComponent('#dependencies'); }} >Dependencies</Link>} />
-    <List.Item content={<Link to="/getting-started#basic-usage" onClick={() => { scrollToComponent('#basic-usage'); }} >Basic Usage</Link>} />
-    <List.Item content={<Link to="/getting-started#configuring-webpack" onClick={() => { scrollToComponent('#configuring-webpack'); }} >Configuring Webpack</Link>} />
-    <List.Item content={<Link to="/getting-started#internationalization-i18n-and-localization-l10n-" onClick={() => { scrollToComponent('#internationalization-i18n-and-localization-l10n-'); }} >I18n</Link>} />
-    <List.Item content={<Link to="/getting-started#theming" onClick={() => { scrollToComponent('#theming'); }} >Theming</Link>} />
-  </List>
-);
-
 export default GettingStartedMenu;
+// layoutConfig: {
+//   size,
+//   toggleMenu: shouldAllowMenuToggle ? this.toggleMenu : undefined,
+//   menuIsOpen,
+//   togglePin: isHoverMenu ? this.togglePin : undefined,
+//   menuIsPinned,
+// }
