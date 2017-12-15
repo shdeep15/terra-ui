f2309961f8de9d98ae0f1ff5ecc9cc850f62303f
Matt Henkes <mjhenkes@gmail.com>
2017-12-14 20:43:19 -0600

    update fetch-config script to not be garbage.

--- scripts/fetch-config.js
+++ scripts/fetch-config.js
@@ -1,24 +1,45 @@
 var https = require('https');
 var fs = require('fs');
 
-// var file = fs.createWriteStream("file.jpg");
-jsonConfig = {};
-var request = https.get("https://raw.githubusercontent.com/cerner/terra-framework/site_revamp/site-examples.json", function(response) {
-  // console.log(response)
-  response.on('data', (d) => {
-    configFragment = JSON.parse(d)
-    // console.log(Object.keys(configFragment)[0])
-    // jsonConfig[Object.keys(configFragment)[0]] =
-    jsonConfig = Object.assign(jsonConfig, configFragment);
-    // json
-    // jsonConfig[](JSON.parse(d));
-    // jsonConfig.push(JSON.parse(d));
-    fs.writeFileSync('./site-example-config/config.json', JSON.stringify(jsonConfig, null, 2));
-    // console.log(jsonConfig)
-    // process.stdout.write(jsonConfig);
-  });
-  // response.pipe(file);
-});
+class FetchConfig {
+  static fetch() {
+    let configs = FetchConfig.repos().map((item) => {
+      return FetchConfig.gatherJson(item);
+    });
+    Promise.all(configs).then((stuff) => {
+      let configJson = stuff.reduce((prevVal, item) => {
+        return Object.assign(prevVal, item);
+      });
 
-// fs.writeFileSync('./site-example-config/config.json', JSON.stringify(jsonConfig, null, 2));
-// response.pipe(file);
+      fs.writeFileSync('./site-example-config/config.json', JSON.stringify(configJson, null, 2));
+    })
+  }
+
+  static repos() {
+    return ['/cerner/terra-framework/site_revamp',];
+  }
+
+  static gatherJson(repo) {
+    return new Promise((resolve, reject) => {
+      const request = https.get(`https://raw.githubusercontent.com${repo}/site-examples.json`, (response) => {
+        // handle http errors
+        if (response.statusCode < 200 || response.statusCode > 299) {
+           reject(new Error('Failed to load page, status code: ' + response.statusCode));
+         }
+        // temporary data holder
+        const body = [];
+        // on every content chunk, push it to the data array
+        response.on('data', (chunk) => body.push(chunk));
+        // we are done, resolve promise with those joined chunks
+        response.on('end', () => {
+          resolve(JSON.parse(body.join('')));
+        });
+
+      });
+
+      request.on('error', (err) => reject(err));
+    });
+  }
+}
+
+FetchConfig.fetch();
